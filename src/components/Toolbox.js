8b3b6c4ca2d287a56ee155bcda1dfffccd0707a0
Gian <gf.fioriello@gmail.com>
2019-04-16 20:30:57 -0300

    pdes #10 - Agrego Toolbox

---
+++ src/components/Toolbox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Toolbox.css';
+
+export const Toolbox = (props) => { 
+  return (
+    <div className="toolbox-contenedor">
+    	Máquinas
+	    <div className="maquinas-contenedor">
+	    </div>
+      Acciones
+	    <div className="acciones-contenedor">
+    	</div>
+    </div>
+  )
+}
--- src/stories/index.js
+++ src/stories/index.js
@@ -5,5 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Fabrica } from '../components/Fabrica.js'
+import { Toolbox } from '../components/Toolbox.js'
 
-storiesOf('Fabrica', module).add('con columnas', () => <Fabrica/>);
+storiesOf('Fabrica', module).add('con columnas', () => <Fabrica />);
+storiesOf('Toolbox', module).add('con maquinas y acciones', () => <Toolbox />);

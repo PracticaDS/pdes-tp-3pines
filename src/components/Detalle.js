7bdc92042664a34e0cec8517ddaeecf913e94607
Gian <gf.fioriello@gmail.com>
2019-04-18 22:07:43 -0300

    pdes #14 - Arreglo estilos en el Detalle

--- src/components/Detalle.js
+++ src/components/Detalle.js
@@ -5,8 +5,14 @@ import '../styles/_compartido.css';
 export const Detalle = (props) => {
   return (
     <div className="detalle-contenedor">
-      <div className="titulo">Detalles</div>
-      <div className="detalle-contenido borde-piola"></div>
+      <div>
+        <div className="titulo">Detalles</div>
+        <div className="detalle-contenido borde-piola">
+          <span className="nombre-maquina"></span>
+          <span className="costo-maquina"></span>
+          <span className="frecuencia-maquina"></span>
+        </div>
+      </div>
     </div>
   )
 }

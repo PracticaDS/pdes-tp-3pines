67d91a704db02aaec3c68283859e431adfdb1b8d
David Correa <david.correa@10pines.com>
2019-04-20 01:53:04 -0300

    Updates

---
+++ precache-manifest.6d9ed4d6c22c4b6c60d9a440562c8c63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64fe24f3d0521ba8eb99",
+    "url": "/pdes-tp-3pines/static/js/runtime~main.bea974cb.js"
+  },
+  {
+    "revision": "1f19f3ac5243eed3461b",
+    "url": "/pdes-tp-3pines/static/js/main.86841ec2.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/pdes-tp-3pines/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1f19f3ac5243eed3461b",
+    "url": "/pdes-tp-3pines/static/css/main.e89caf48.chunk.css"
+  },
+  {
+    "revision": "c526490cd0f39ca157f721f78af691de",
+    "url": "/pdes-tp-3pines/index.html"
+  }
+];

b8466c8dabbba37ddcc3611803b1804cb78806cf
Gian <gf.fioriello@gmail.com>
2019-04-22 20:32:16 -0300

    pdes #14 - Modifico selector borde_piola a borde_redondiado y muevo un if de lugar

--- src/components/Toolbox.js
+++ src/components/Toolbox.js
@@ -7,12 +7,12 @@ export const Toolbox = (props) => {
     <div className="toolbox-contenedor">
       <div className="acciones-contenedor">
       	<div className="titulo">Máquinas</div>
-  	    <div className="toolbox-acciones borde-piola">
+  	    <div className="toolbox-acciones borde-redondeado">
       </div>
 	    </div>
       <div className="acciones-contenedor">
         <div className="titulo">Edición</div>
-  	    <div className="toolbox-acciones borde-piola">
+  	    <div className="toolbox-acciones borde-redondeado">
       </div>
     	</div>
     </div>

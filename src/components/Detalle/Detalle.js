125c0b4caf1386ebafb4ad2422da5061a22df640
Gian <gf.fioriello@gmail.com>
2019-04-28 20:34:08 -0300

    pdes #35 - agrego DetalleContainer - paso la maquina seleccionada (si la hay) por props para mostrar sus detalles

---
+++ src/components/Detalle/Detalle.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../styles/Detalle.scss';
+import '../../styles/_compartido.scss';
+
+export const Detalle = ({maquina}) => {
+  return (
+    <div className="detalle-contenedor">
+      <div>
+        <div className="titulo">Detalles</div>
+            <div className="detalle-contenido borde-redondeado">
+            {
+              maquina ?
+              <div>
+                <div className="nombre-maquina">{maquina.nombre}</div>
+                <div className="costo-maquina">Costo: <span> ${maquina.costo} </span></div>
+                <div className="frecuencia-maquina">Frecuencia: <span> {maquina.frecuencia}/s </span></div>
+              </div> :
+              ''
+            }
+            </div>
+      </div>
+    </div>
+  )
+}

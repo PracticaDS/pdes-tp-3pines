870194e147f3918f3167c64036cc8f04650cb699
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-07 22:52:02 +0000

    Deploy PracticaDS/pdes-tp-3pines to github.com/PracticaDS/pdes-tp-3pines.git:gh-pages

---
+++ precache-manifest.ca7bc5bba44988375532725fa30f8948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64fe24f3d0521ba8eb99",
+    "url": "/pdes-tp-3pines/static/js/runtime~main.bea974cb.js"
+  },
+  {
+    "revision": "321fd0762a114f7fb772",
+    "url": "/pdes-tp-3pines/static/js/main.30a2e343.chunk.js"
+  },
+  {
+    "revision": "311a5c7a83e4208f6f89",
+    "url": "/pdes-tp-3pines/static/js/2.5400981b.chunk.js"
+  },
+  {
+    "revision": "321fd0762a114f7fb772",
+    "url": "/pdes-tp-3pines/static/css/main.ee817fe8.chunk.css"
+  },
+  {
+    "revision": "fca1684c1ccae47dc56352d4bfa257ff",
+    "url": "/pdes-tp-3pines/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9a966126f7861f287254bd5811e3158a.js"
+  "/pdes-tp-3pines/precache-manifest.ca7bc5bba44988375532725fa30f8948.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/pdes-tp-3pines/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

9aa4f62f31cb704caed2c440dbac08961c2c89df
gian <gf.fioriello@gmail.com>
2019-05-07 21:28:39 -0300

    Refactorizamos las celdas

--- src/reducers/fabrica.js
+++ src/reducers/fabrica.js
@@ -1,6 +1,7 @@
 import {AGREGAR_MAQUINA, SELECCIONAR_CELDA} from '../actions/seleccionarCelda';
 import {MAQUINAS} from "../constantes";
 import generarCeldas from './generadorDeCeldas'
+import { seleccionar, deseleccionar, esIgualA, asignarMaquina } from '../models/Celda'
 
 const estadoInicial = {
   celdas: generarCeldas(10, 10),
@@ -10,20 +11,19 @@ function reducer(estado = estadoInicial, { type, payload }) {
   switch (type) {
     case SELECCIONAR_CELDA: {
       const nuevasCeldas = [...estado.celdas].map(celda => {
-        if(celda.esIgualA(payload)) {
-          celda.seleccionar()
+        if(esIgualA(celda, payload)) {
+          return seleccionar(celda)
         } else {
-          celda.deseleccionar()
+          return deseleccionar(celda)
         } 
-        return celda 
       })
       return { ...estado, celdas: nuevasCeldas };
     }
     case AGREGAR_MAQUINA: {
       const maquinaAAgregar = MAQUINAS.find(({ nombre }) => nombre === payload.maquinaAAgregar)
       const nuevasCeldas = [...estado.celdas].map( celda => {
-        if(celda.esIgualA(payload.celda)) {
-          celda.asignarMaquina(maquinaAAgregar)
+        if(esIgualA(celda, payload.celda)) {
+          return asignarMaquina(celda, maquinaAAgregar)
         }
         return celda
       })

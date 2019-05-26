85faa970c56139b60da5f42196ceaf8355427156
Gian <gf.fioriello@gmail.com>
2019-05-26 19:46:14 -0300

    #62 - Agrego server

--- back-end/index.js
+++ back-end/index.js
@@ -1,3 +1,9 @@
-const helloWorld = () => console.log('Hola Mundo!')
+import app from './src/server'
 
-helloWorld()
+const isProduction = process.env.NODE_ENV === 'production'
+const port = isProduction ? process.env.PORT : 3001
+
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Server running on port ${port}`)
+})

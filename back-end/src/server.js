8141fefe5c8b2c9323c7cbc9a23c53d21166f665
Gian <gf.fioriello@gmail.com>
2019-05-26 19:54:38 -0300

    pdes #62 - Agrego Router con su test en supertest

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import morgan from 'morgan'
+import api from './router/api'
 
 const app = express()
 app.use(morgan('dev'))
-
-app.get('/', (req, res) => {
-  res.status(200).json({ status: 'ok', greeting: 'hello' })
-})
+app.use('/api', api)
 
 export default app

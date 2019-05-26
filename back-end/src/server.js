dc6f135fdc4e7a4a2f2317398246ad955af0bec0
Gian <gf.fioriello@gmail.com>
2019-05-26 19:50:08 -0300

    #62 - Agrego logger

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.status(200).send('hello')
+  res.status(200).json({ status: 'ok', greeting: 'hello' })
 })
 
 export default app

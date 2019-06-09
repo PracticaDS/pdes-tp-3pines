2d576f672107926f8f30d6a60bcbd59cf247ffd1
mariabelenamat <belen.amat29@gmail.com>
2019-06-09 17:31:40 -0300

    agrego aserciones de chai

--- back-end/src/router/api.spec.js
+++ back-end/src/router/api.spec.js
@@ -1,29 +1,42 @@
 import request from 'supertest'
+const { OK, CREATED, NOT_FOUND } = require('http-status-codes');
 import app from '../server'
+import chai from 'chai'
+
+const { assert } = chai;
 
 describe('API', () => {
 
-  it('/api devuelve ok', () => {
-    
-    return request(app)
-      .get('/')
-      .expect(200, { status: 'ok' })
-        .done();
-  });
+    it('/api devuelve ok', (done) => {
+        request(app)
+            .get('/')
+            .expect(OK)
+            .end(done);
+    })
 
-  describe('/:usuario', () => {
+    describe('/:usuario', () => {
+        it('Creates and return a new user', (done) => {
+            request(app)
+                .get('/pepe')
+                .expect(OK)
+                .expect((res) => {
+                    assert.include(res.body.usuario, {nombre: 'pepe'});
+                })
+                .end(done);
+        })
 
-    it('devuelve el usuario con el nombre del query param usuario', () => {
+    });
+})
+
+   /* it('devuelve el usuario con el nombre del query param usuario', () => {
       const usuario = { nombre: 'pepe'};
       let nombreDeUsuario = null;
-      return request(app)
+      request(app)
         .get('/pepe')
           .then(response => {
             expect(response.status).toBe(200)
             expect(response.body.usuario.nombre).toEqual('pepe')
           })
-        done()
     })
-  })
-    afterAll(() => mongoose.disconnect());
-})
+  })*/
+

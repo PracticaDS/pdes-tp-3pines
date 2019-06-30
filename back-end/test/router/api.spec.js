ef06ac0b0afa5a8fb6a058ffd06cfa4aa28af8ca
Gian <gf.fioriello@gmail.com>
2019-06-30 17:50:38 -0300

    WIP

--- back-end/test/router/api.spec.js
+++ back-end/test/router/api.spec.js
@@ -3,10 +3,27 @@ import request from 'supertest'
 import { OK } from 'http-status-codes'
 import app from '../../src/server'
 import chai from 'chai'
+import FabricaApp from '../../src/model/fabrica-app';
 
 const { assert } = chai;
 
 describe('API', () => {
+  let nombreUsuario
+  let fabricaInicial
+  let usuario
+
+  beforeEach(() => {
+    nombreUsuario = 'pepe'
+    usuario = { nombre: nombreUsuario }
+    fabricaInicial = {
+      ancho: 10,
+      alto: 10,
+      celdas: [],  // TODO: generar lista de celdas.
+      ganancia: 0,
+      nombreUsuario
+    }
+  })
+
   it('/api devuelve ok', (done) => {
     request(app)
       .get('/api')
@@ -15,26 +32,94 @@ describe('API', () => {
   })
 
   describe('/login', () => {
-    it('Logea el usuario y devuelve una fabrica vacia', (done) => {
-      request(app)
-        .post('/api/login')
-        .send({ usuario: { nombre: 'pepe' }})
-        .expect(OK)
-        .expect((res) => {
-          assert.include(res.body, []);
-        })
-        .end(done);
-    })
+
+    describe('para un usuario nuevo', () => {
+
+      it('logea el usuario y devuelve una fabrica nueva', (done) => {
+        request(app)
+          .post('/api/login')
+          .send(usuario)
+          .expect(OK)
+          .expect((res) => {
+             assert.equal(res.body.fabrica.nombreUsuario, nombreUsuario)
+             assert.equal(res.body.fabrica.ganancia, 0)
+          })
+          .end(done);
+      })
+    });
+  
+    describe('para un usuario existente', () => {
+
+      it('devuelve la fabrica del usuario', async (done) => {
+        const fabrica = {...fabricaInicial, ganancia: 10 }
+
+        await request(app)
+          .post('/api/login')
+          .send(usuario)
+
+        await request(app)
+          .post('/api/fabrica')
+          .send({ usuario: nombreUsuario , fabrica})
+
+        request(app)
+          .post('/api/login')
+          .send(usuario)
+          .expect(OK)
+          .expect((res) => {
+             assert.equal(res.body.fabrica.nombreUsuario, nombreUsuario)
+             assert.equal(res.body.fabrica.ganancia, fabrica.ganancia)
+          })
+          .end(done);
+      })
+
+      it('no lo vuelve a guardar', async (done) => {
+        let id
+
+        await request(app)
+          .post('/api/login')
+          .send(usuario)
+          .expect(OK)
+          .expect((res) => {
+            assert.equal(res.body.fabrica.nombreUsuario, nombreUsuario)
+            id = res.body.fabrica._id
+          })
+
+        request(app)
+          .post('/api/login')
+          .send(usuario)
+          .expect(OK)
+          .expect((res) => {
+            assert.equal(res.body.fabrica.nombreUsuario, nombreUsuario)
+            assert.equal(res.body.fabrica._id, id)
+          })
+          .end(done);
+      })
+    });
   });
 
   describe('/fabrica', () => {
-    it('Guarda la fabrica', (done) => {
-      request(app)
-        .post('/api/fabrica')
-        .send({ usuario: 'Pepe' , fabrica: {}})
+
+    it('guarda la fabrica', async (done) => {
+      let idDeFabrica
+      await request(app)
+        .post('/api/login')
+        .send(usuario)
         .expect(OK)
         .expect((res) => {
-          assert.include(res.body, []);
+          assert.equal(res.body.fabrica.nombreUsuario, nombreUsuario)
+          idDeFabrica = res.body.fabrica._id
+        })
+
+      const fabrica = {...fabricaInicial, ganancia: 10 }
+
+      request(app)
+        .post('/api/fabrica')
+        .send({ usuario , fabrica})
+        .expect(OK)
+        .expect((res) => {
+          assert.equal(res.body.fabrica.nombreUsuario, nombreUsuario)
+          assert.equal(res.body.fabrica.ganancia, fabrica.ganancia)
+          assert.equal(res.body.fabrica._id, idDeFabrica)
         })
         .end(done);
     })
